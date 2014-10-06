a005e31fd1d139ace378cdd2028e3fd2be3a8342
kbaba1001 <kbaba1001@gmail.com>
2014-10-07 00:33:34 +0900

    scenario "Add new idea"

--- test/features/add_new_idea_test.rb
+++ test/features/add_new_idea_test.rb
@@ -2,8 +2,21 @@ require 'test_helper'
 
 feature 'Add new idea' do
   scenario 'has listing ideas' do
-    visit '/ideas'
+    visit('/ideas')
 
-    page.must_have_content 'Listing ideas'
+    page.must_have_content('Listing ideas')
+  end
+
+  scenario 'Add new idea' do
+    visit('/ideas')
+    click_on('New Idea')
+    page.must_have_content('New idea')
+
+    fill_in('Name', with: 'すごいアイディア')
+    fill_in('Description', with: 'プリンに醤油をかけるとウニの味がする')
+    fill_in('Picture', with: 'pudding_shoyu.jpg')
+    click_on('Save')
+
+    page.must_have_content('Idea was successfully created.')
   end
 end
