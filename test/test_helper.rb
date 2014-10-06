6363c8222d05146985a4f09a7645a22bc1ff6f87
kbaba1001 <kbaba1001@gmail.com>
2014-10-06 23:37:32 +0900

    cosmetics

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,8 @@
-ENV["RAILS_ENV"] = "test"
-require File.expand_path("../../config/environment", __FILE__)
-require "rails/test_help"
-require "minitest/rails"
-require "minitest/reporters"
+ENV['RAILS_ENV'] = 'test'
+require File.expand_path('../../config/environment', __FILE__)
+require 'rails/test_help'
+require 'minitest/rails'
+require 'minitest/reporters'
 
 Minitest::Reporters.use!(Minitest::Reporters::ProgressReporter.new)
 
