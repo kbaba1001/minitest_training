97773bdd3c060903418150920a8deaaaab093138
kbaba1001 <kbaba1001@gmail.com>
2014-10-07 00:15:53 +0900

    cosmetics

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,10 +7,6 @@ require 'minitest/rails/capybara'
 
 Minitest::Reporters.use!(Minitest::Reporters::ProgressReporter.new)
 
-# To add Capybara feature tests add `gem "minitest-rails-capybara"`
-# to the test group in the Gemfile and uncomment the following:
-# require "minitest/rails/capybara"
-
 # Uncomment for awesome colorful output
 # require "minitest/pride"
 
