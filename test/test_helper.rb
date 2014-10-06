2380cb40e2f6af09968e5806aa715d5aabf5b754
kbaba1001 <kbaba1001@gmail.com>
2014-10-06 23:41:45 +0900

    require 'minitest/rails/capybara'

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,7 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'minitest/rails'
 require 'minitest/reporters'
+require 'minitest/rails/capybara'
 
 Minitest::Reporters.use!(Minitest::Reporters::ProgressReporter.new)
 
